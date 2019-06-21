1df6201386903bf57a9fd8ae2ecf46a16564657b
Amir Bizimana <amirbzmn@gmail.com>
2019-06-21 01:42:44 -0400

    with router to pokemon component(still in progress)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
-
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/layout/Dashboard";
+import Pokemon from "./components/pokemon/Pokemon";
+
+import backgroundImage from "./pattern.png";
 
 class App extends Component {
   state = {};
 
   render() {
     return (
-      <React.Fragment>
-        <div className="App">
+      <Router>
+        <div className="App" style={{ background: `url(${backgroundImage})` }}>
           <Navbar />
           <div className="container">
-            <Dashboard />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
           </div>
         </div>
-      </React.Fragment>
+      </Router>
     );
   }
 }
--- src/components/pokemon/PokemonList.jsx
+++ src/components/pokemon/PokemonList.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 import PokemonCard from "./PokemonCard";
 
 class PokemonList extends Component {
   state = {
-    url: 'https://pokeapi.co/api/v2/pokemon/',
-    pokemon:null
+    url: "https://pokeapi.co/api/v2/pokemon/?limit=150",
+    pokemon: null
   };
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data["results"] });
+  }
   render() {
     return (
       <React.Fragment>
-        <div className="row">
-          <PokemonCard />
-          <PokemonCard />
-          <PokemonCard />
-          <PokemonCard />
-          <PokemonCard />
-        </div>
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map(pokemon => (
+              <PokemonCard
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokemon</h1>
+        )}
       </React.Fragment>
     );
   }
